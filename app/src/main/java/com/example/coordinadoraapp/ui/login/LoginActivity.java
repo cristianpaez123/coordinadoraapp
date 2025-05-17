905e9083baa959aa6f30b105bc194d2f1020e5b0
Cristian Paez <cristianpaezguerrero@gmail.com>
2025-05-17 18:00:47 -0500

    organize LoginActivity

--- app/src/main/java/com/example/coordinadoraapp/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/coordinadoraapp/ui/login/LoginActivity.java
@@ -24,19 +24,28 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot()); // <-- Este reemplaza setContentView(R.layout...)
 
         ((MyApplication) getApplication()).appComponent.inject(this);
 
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         viewModel = new ViewModelProvider(this, factory).get(LoginViewModel.class);
 
+        setupListeners();
+
+        setupObservers();
+    }
+
+    private void setupListeners() {
         binding.btnLogin.setOnClickListener(v -> {
             String email = binding.editTextEmail.getText().toString().trim();
             String password = binding.editTextPassword.getText().toString().trim();
             viewModel.login(email, password);
         });
+    }
 
+    private void setupObservers() {
         viewModel.loginSuccess.observe(this, success -> {
             if (success != null && success) {
                 startActivity(new Intent(this, MainActivity.class));
