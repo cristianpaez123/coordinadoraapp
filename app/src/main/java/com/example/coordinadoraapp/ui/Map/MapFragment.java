a3d392db32ae3051a85ad027d1636f116a76faa9
Cristian Paez <cristianpaezguerrero@gmail.com>
2025-05-19 08:50:38 -0500

    refactor

--- app/src/main/java/com/example/coordinadoraapp/ui/Map/MapFragment.java
+++ app/src/main/java/com/example/coordinadoraapp/ui/Map/MapFragment.java
@@ -1,15 +1,11 @@
 package com.example.coordinadoraapp.ui.Map;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
